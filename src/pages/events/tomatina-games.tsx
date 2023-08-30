6515b8d8a98b3e1583ef73f7f5bc5250f061be8e
ふぁー <47295014+ymuichiro@users.noreply.github.com>
2023-08-30 13:10:17 +0900

    [add] games

--- src/pages/events/tomatina-games.tsx
+++ src/pages/events/tomatina-games.tsx
@@ -6,27 +6,20 @@ import TomatinaTwitterHeader from '@/assets/events/tomatina/symbol-tomatina-head
 import TomatinaGraphImage from '@/assets/events/tomatina/tomatina-graph.webp';
 import TomatinaHirobaImage from '@/assets/events/tomatina/tomatina-hiroba.webp';
 import TomatinaOpeningLine from '@/assets/events/tomatina/tomatina-opening-line.webp';
-import TomatoFaceImage from '@/assets/events/tomatina/tomato-face.png';
 import ThrowTomatoImage from '@/assets/events/tomatina/tomato-icon.webp';
 import TomatoVsPotato from '@/assets/events/tomatina/tomato-vs-potato.webp';
 import TomatoImage from '@/assets/events/tomatina/tomato.png';
 import TreasureImage from '@/assets/events/tomatina/treasure.webp';
 import WellcomeToSymbolTomatina from '@/assets/events/tomatina/wellcome-to-symbol-tomatina.webp';
 import InViewAnimation from '@/components/atom/InViewAnimation';
-import LinkButton from '@/components/atom/LinkButton';
 import Footer from '@/components/moleculs/Footer';
 import Header from '@/components/moleculs/Header';
 import MediaCard from '@/components/moleculs/MediaCard';
 import { lang, langSelecter } from '@/languages';
 import styled from '@emotion/styled';
-import Button from '@mui/material/Button';
 import ButtonBase from '@mui/material/ButtonBase';
-import Card from '@mui/material/Card';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -83,10 +76,10 @@ const TomatinaEvent: NextPage<Props> = ({ i18n }) => {
   return (
     <>
       <Head>
-        <title>{`Tomatina | ${i18n.meta_page_title}`}</title>
-        <meta name='description' content={'Symbol トマティーナのイベントページです'} />
+        <title>{`Tomatina Games | ${i18n.meta_page_title}`}</title>
+        <meta name='description' content={'Symbol トマティーナで用意されたゲーム一覧です'} />
         <meta name='twitter:title' content={`Tomatina | ${i18n.meta_page_title}`} />
-        <meta name='twitter:description' content={'Symbol トマティーナのイベントページです'} />
+        <meta name='twitter:description' content={'Symbol トマティーナで用意されたゲーム一覧です'} />
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:image' content={`${process.env.NEXT_PUBLIC_HOSTING_URL}${TomatinaTwitterHeader.src}`} />
       </Head>
@@ -111,156 +104,10 @@ const TomatinaEvent: NextPage<Props> = ({ i18n }) => {
           backgroundPosition: 'center bottom',
         }}
       />
-      <Container maxWidth='lg' style={{ marginBottom: '5vh' }}>
-        <div
-          style={{
-            height: '80svh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            marginBottom: '20svh',
-          }}
-        >
-          <Typography
-            variant='h1'
-            fontWeight='bold'
-            textAlign='center'
-            gutterBottom
-            sx={{ fontSize: { xs: '3rem', sm: '6rem', md: '8rem', lg: '10rem' } }}
-          >
-            Symbol Tomatina
-          </Typography>
-          <Typography variant='h5' fontWeight='bold' textAlign='center'>
-            熱したデジタルトマトをぶつけ合おう
-          </Typography>
-        </div>
-        <InViewAnimation direction='up' threshold={0.2}>
-          <Typography variant='h4' fontWeight={'bold'} gutterBottom>
-            デジタルトマトをぶつけまくる
-          </Typography>
-          <Grid container spacing={3} alignItems={'center'} style={{ minHeight: '50svh' }}>
-            <Grid item xs={12} md={7}>
-              <Typography variant='body1' style={{ marginBottom: '1rem' }}>
-                このイベントでは期間限定でトマトを模したデジタルトークンが発行され、無料にて配布されます。配布されたデジタルトマトトークンを使って見知らぬ誰かにブロックチェーンのトランザクションとして投げつけたり、トマトを投げつけるようなゲームをプレイする事ができます。
-              </Typography>
-              <LinkButton
-                href='https://docs.symbol.dev/ja/concepts/mosaic.html'
-                ariaLabel='mosaic link'
-                isNewTab
-                variant='outlined'
-                sx={{ width: { xs: '100%', sm: '100%', md: '300px' } }}
-              >
-                デジタルトークンについて知る
-              </LinkButton>
-            </Grid>
-            <Grid item xs={12} md={5}>
-              <div
-                style={{
-                  width: '100%',
-                  textAlign: 'center',
-                  marginTop: '5svh',
-                  marginBottom: '5svh',
-                  overflow: 'clip',
-                }}
-              >
-                <Image
-                  src={TomatoFaceImage}
-                  height={250}
-                  width={250}
-                  alt='tomato image'
-                  draggable
-                  onDragStart={() => setDraggedElement('j2dj1ka')}
-                  onDragEnd={() => setDraggedElement('')}
-                />
-              </div>
-            </Grid>
-          </Grid>
-        </InViewAnimation>
-        <div style={{ height: '5svh' }} />
-        <InViewAnimation direction='up' threshold={0.2}>
-          <Typography variant='h4' fontWeight={'bold'} textAlign={'center'} gutterBottom>
-            以前の開催の様子
-          </Typography>
-          <Grid container spacing={3} alignItems={'center'} style={{ minHeight: '50svh' }}>
-            <Grid item xs={12} md={4}>
-              <Typography variant='body1' style={{ marginBottom: '1rem' }}>
-                オンラインでトマトをぶつけあうゲームをプレイしたり、RPGゲーム等が公開されました。
-                トマトを投げると実際にトランザクションが作成され、ブロックチェーン上に結果が記録されるブロックチェーンゲームです。
-              </Typography>
-            </Grid>
-            <Grid item xs={12} md={8}>
-              <div style={{ width: '100%', textAlign: 'center', marginTop: '5svh', marginBottom: '5svh' }}>
-                <iframe
-                  width='560'
-                  height='315'
-                  src='https://www.youtube.com/embed/mebNYjgFMms'
-                  title='YouTube video player'
-                  allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
-                  allowFullScreen={true}
-                  style={{ border: 0, maxWidth: '100%' }}
-                ></iframe>
-              </div>
-            </Grid>
-          </Grid>
-        </InViewAnimation>
-        <InViewAnimation direction='up' threshold={0.2}>
-          <Typography variant='h4' fontWeight={'bold'} textAlign={'center'} gutterBottom>
-            事前準備
-          </Typography>
-          <Typography variant='body1' style={{ marginBottom: '3rem', marginTop: '3rem' }}>
-            公開されているゲームは Wallet なしでプレイできるものもありますが、当日慌てない為にも事前に Wallet
-            を作成しておきましょう。
-          </Typography>
-          <Grid container spacing={3}>
-            <Grid item xs={12} sm={12} md={6}>
-              <Button
-                variant='outlined'
-                fullWidth
-                LinkComponent={Link}
-                href='https://coin-view.net/symbolwallet-mobile/'
-              >
-                モバイルウォレット-1
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={12} md={6}>
-              <Button
-                variant='outlined'
-                fullWidth
-                LinkComponent={Link}
-                href='https://www.aemalgorithm.io/aem-plus/main'
-              >
-                モバイルウォレット-2
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={12} md={6}>
-              <Button
-                variant='outlined'
-                fullWidth
-                LinkComponent={Link}
-                href='https://docs.symbol.dev/ja/wallets.html#wallet-desktop'
-              >
-                デスクトップウォレット
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={12} md={6}>
-              <Button
-                fullWidth
-                variant='outlined'
-                LinkComponent={Link}
-                href='https://chrome.google.com/webstore/detail/sss-extension/llildiojemakefgnhhkmiiffonembcan?hl=ja'
-              >
-                ブラウザ拡張署名ツール
-              </Button>
-            </Grid>
-          </Grid>
-        </InViewAnimation>
-        <div style={{ height: '5svh' }} />
-        <InViewAnimation direction='right' threshold={0.2}>
-          <Typography variant='h4' fontWeight={'bold'} gutterBottom>
-            ゲーム楽しもう
-          </Typography>
-        </InViewAnimation>
+      <Container maxWidth='lg' style={{ marginBottom: '5vh', marginTop: '5vh' }}>
+        <Typography variant='h4' fontWeight={'bold'} gutterBottom>
+          ゲーム楽しもう
+        </Typography>
         <Typography variant='body1' style={{ marginBottom: '1rem' }}>
           現在公開されているゲームを掲載しております。プレイ方法、ルールなどは各ゲームのページをご覧ください。
         </Typography>
@@ -419,45 +266,6 @@ const TomatinaEvent: NextPage<Props> = ({ i18n }) => {
           </div>
         </InViewAnimation>
       </Container>
-      <Card style={{ marginTop: '10svh', width: '100%', borderRadius: 0, paddingTop: '5svh', paddingBottom: '5svh' }}>
-        <Container maxWidth={'lg'}>
-          <Typography variant='h4' fontWeight={'bold'} gutterBottom>
-            開催概要
-          </Typography>
-          <Typography variant='body1' style={{ marginBottom: '1rem' }}>
-            2023年度 Symbol Tomatina の開催概要を掲載します。（準備中）
-          </Typography>
-          <Grid container alignItems={'center'}>
-            <Grid item xs={12} md={6}>
-              <List style={{ flexGrow: 1 }} disablePadding>
-                <ListItem>
-                  <ListItemText primary='開催日時' secondary='毎年８月最終水曜日' />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary='開催場所' secondary='Twitter等' />
-                </ListItem>
-              </List>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <List style={{ flexGrow: 1 }} disablePadding>
-                <ListItem>
-                  <ListItemText
-                    primary='問い合わせ'
-                    secondary={
-                      <Link href={'https://discord.gg/TT2tvxFfN4'} style={{ color: 'white' }}>
-                        Symbol/NEM Marketing
-                      </Link>
-                    }
-                  />
-                </ListItem>
-                <ListItem>
-                  <ListItemText primary='Tomato Mosaic ID' secondary='各自自由に発行' />
-                </ListItem>
-              </List>
-            </Grid>
-          </Grid>
-        </Container>
-      </Card>
       <section style={{ marginTop: '100px' }}>
         <Container maxWidth={'lg'}>
           <Footer />
